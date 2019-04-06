315b0252e76522e3369b328b84d16446462f17ad
Pedro Teixeira <pedro.teix@ua.pt>
2019-04-06 23:52:16 +0100

    Initial Version of Home Page (added calendar). Fixed bugs on navbar

--- app/src/main/java/com/yacm/yacm/MainActivity.java
+++ app/src/main/java/com/yacm/yacm/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,16 +19,45 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    /* go to home */
+
+                    if (BuildConfig.DEBUG) {
+                        Toast.makeText(getApplicationContext(), "Home", Toast.LENGTH_SHORT).show();
+                    }
+
+                    // mTextMessage.setText(R.string.title_home);
+
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    /* go to dashboard */
+
+                    if (BuildConfig.DEBUG) {
+                        Toast.makeText(getApplicationContext(), R.string.title_dashboard, Toast.LENGTH_SHORT).show();
+                    }
+
+                    // mTextMessage.setText(R.string.title_dashboard);
+
                     return true;
+
                 case R.id.navigation_search:
-                    mTextMessage.setText("Search");
+                    /* go to search */
+
+                    if (BuildConfig.DEBUG) {
+                        Toast.makeText(getApplicationContext(), R.string.title_search, Toast.LENGTH_SHORT).show();
+                    }
+
+                    //mTextMessage.setText("Search");
+
                     return true;
+
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    /* go to notifications */
+
+                    if (BuildConfig.DEBUG) {
+                        Toast.makeText(getApplicationContext(), R.string.title_notifications, Toast.LENGTH_SHORT).show();
+                    }
+
+                    // mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
