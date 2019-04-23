452ccc0e8fd2a8cdec31c6551511aeb1fef4eb9b
Pedro Teixeira <pedro.teix@ua.pt>
2019-04-23 10:05:36 +0100

    Fixed Hambuger Menu. Cleanup

--- app/src/main/java/com/ihc/yacm/MainActivity.java
+++ app/src/main/java/com/ihc/yacm/MainActivity.java
@@ -19,6 +19,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -89,18 +90,22 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_home) {
+            if (BuildConfig.DEBUG) {
+                Toast.makeText(getApplicationContext(), "Home", Toast.LENGTH_SHORT).show();
+            }
+        } else if (id == R.id.nav_notifications) {
+            if (BuildConfig.DEBUG) {
+                Toast.makeText(getApplicationContext(), "Notifications", Toast.LENGTH_SHORT).show();
+            }
+        } else if (id == R.id.nav_account) {
+            if (BuildConfig.DEBUG) {
+                Toast.makeText(getApplicationContext(), "Account", Toast.LENGTH_SHORT).show();
+            }
+        } else if (id == R.id.nav_logout) {
+            if (BuildConfig.DEBUG) {
+                Toast.makeText(getApplicationContext(), "Logout", Toast.LENGTH_SHORT).show();
+            }
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -115,7 +120,11 @@ public class MainActivity extends AppCompatActivity
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                   // mTextMessage.setText(R.string.title_home);
+                    // mTextMessage.setText(R.string.title_home);
+                    if (BuildConfig.DEBUG) {
+                        // do something for a debug build
+                        Toast.makeText(MainActivity.this,"test", Toast.LENGTH_SHORT).show();
+                    }
                     return true;
                 case R.id.navigation_dashboard:
                    // mTextMessage.setText(R.string.title_dashboard);
