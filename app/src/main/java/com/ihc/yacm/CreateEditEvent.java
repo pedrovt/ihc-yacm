5e7b0fc474d78465aa35bfb1cdb24f2385467b17
Pedro Teixeira <pedro.teix@ua.pt>
2019-05-13 17:23:16 +0100

    Added Create/Edit Event Layout

---
+++ app/src/main/java/com/ihc/yacm/CreateEditEvent.java
@@ -0,0 +1,14 @@
+package com.ihc.yacm;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateEditEvent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_edit_event);
+    }
+}
