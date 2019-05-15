f84bb20ff43ed7c75e6e51d25998f8aaa60a705c
Paulo Vasconcelos <paulobvasconcelos@gmail.com>
2019-05-15 22:17:47 +0100

    Me likes back-end, screw xml

--- app/src/main/java/com/ihc/yacm/ui/login/LoggedInUserView.java
+++ app/src/main/java/com/ihc/yacm/ui/login/LoggedInUserView.java
@@ -4,14 +4,31 @@ package com.ihc.yacm.ui.login;
  * Class exposing authenticated user details to the UI.
  */
 class LoggedInUserView {
-    private String displayName;
+    private String displayName = "";
+    private String emailName = "";
+    private String emailServiceProvider = "";
     //... other data fields that may be accessible to the UI
 
     LoggedInUserView(String displayName) {
         this.displayName = displayName;
     }
 
+    LoggedInUserView(String displayName,String email) {
+        assert email.indexOf('@')>=0;
+        this.displayName = displayName;
+        this.emailName = email.substring(0,email.indexOf('@'));
+        this.emailServiceProvider = email.substring(email.indexOf('@')+1);
+    }
+
     String getDisplayName() {
         return displayName;
+    }
+
+    String getValidID() {
+        if (displayName!="")
+            return displayName;
+        if (emailName!="" && emailServiceProvider!="")
+            return emailName+"@"+emailServiceProvider;
+        return "";
     }
 }
