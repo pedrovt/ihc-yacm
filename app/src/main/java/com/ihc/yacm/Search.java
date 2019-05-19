40979985fea759a50c38359d936127c5adc15cb1
Pedro Teixeira <pedro.teix@ua.pt>
2019-05-19 21:02:22 +0100

    Fix to ensure it can compile

--- app/src/main/java/com/ihc/yacm/Search.java
+++ app/src/main/java/com/ihc/yacm/Search.java
@@ -1,13 +1,12 @@
 package com.ihc.yacm;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.ImageButton;
-import android.view.View;
-
 import com.google.android.material.textfield.TextInputEditText;
-import com.google.android.material.textfield.TextInputLayout;
 
 public class Search extends AppCompatActivity {
 
@@ -15,14 +14,16 @@ public class Search extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                //commented to ensure build
+                // String keywords = keywordsBox.getEditText().getText().toString();
+            }
+        });
     }
 
     ImageButton searchButton = (ImageButton) findViewById(R.id.imageButton);
     TextInputEditText keywordsBox = (TextInputEditText) findViewById(R.id.keywords);
 
-    searchButton.setOnClickListener(new View.OnClickListener() {
-        public void onClick(View view) {
-            String keywords = keywordsBox.getEditText().getText().toString();
-        }
-    });
+
 }
