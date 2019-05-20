b0e1b110306d68b2e1eb73bb343ce71b3e2ee028
Pedro Teixeira <pedro.teix@ua.pt>
2019-05-20 21:38:08 +0100

    Trying to fix the Create Event

--- app/src/main/java/com/ihc/yacm/CreateEditEvent.java
+++ app/src/main/java/com/ihc/yacm/CreateEditEvent.java
@@ -44,6 +44,34 @@ public class CreateEditEvent extends AppCompatActivity {
             }
         });
         setTitle("Create Event");
+
+        // TODO FIXME refactor this CTOR to use the more general one
+        BottomNavigationView bottomNav = (BottomNavigationView) findViewById(R.id.navigation);
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        //Toast.makeText(Home.this,"Home", Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(getApplicationContext(), Home.class);
+                        intent.putExtra("Title", "Event Details");
+                        startActivity(intent);
+                        return true;
+                    case R.id.navigation_dashboard:
+                        //Toast.makeText(Home.this,"Create Event", Toast.LENGTH_LONG).show();
+                        intent = new Intent(getApplicationContext(), CreateEditEvent.class);
+                        startActivity(intent);
+                        return true;
+                    case R.id.navigation_notifications:
+                        //Toast.makeText(Home.this,"Notifications", Toast.LENGTH_LONG).show();
+                        intent = new Intent(getApplicationContext(), Notifications.class);
+                        intent.putExtra("Title", "Event Details");
+                        startActivity(intent);
+                        return true;
+                }
+                return false;
+            };
+        });
     }
 
     protected void onCreate(Bundle savedInstanceState, boolean create) {
@@ -113,21 +141,24 @@ public class CreateEditEvent extends AppCompatActivity {
         String eventsName = preferences.getString("events", null);     //names
         eventsName.concat("," + eventName);
 
+        editor.putString("events", eventsName);
         editor.putString("event" + i + "_name", eventName);             //name
         editor.putString("event" + i + "_date", eventDate);             //date
         editor.putString("event" + i + "_location", eventLocation);     //date
         editor.putString("event" + i + "_tags", eventTags);             //tags
+
+        editor.apply();
 
     }
 
     private void editEvent(int eventIndex, String eventName, String eventDate, String eventLocation, String eventTags) {
         SharedPreferences preferences = getSharedPreferences(Utils.PREFERENCES, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
-        
 
         editor.putString("event" + eventIndex + "_date", eventDate);             //date
         editor.putString("event" + eventIndex + "_location", eventLocation);     //date
         editor.putString("event" + eventIndex + "_tags", eventTags);             //tags
 
+        editor.apply();
     }
 }
