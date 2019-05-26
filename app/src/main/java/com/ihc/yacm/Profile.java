087921a2900f98bdc4ce08736629009bfb6b4c0e
Paulo Vasconcelos <paulobvasconcelos@gmail.com>
2019-05-26 22:43:30 +0100

    Quim Barreiros as Static Profile

--- app/src/main/java/com/ihc/yacm/Profile.java
+++ app/src/main/java/com/ihc/yacm/Profile.java
@@ -1,8 +1,14 @@
 package com.ihc.yacm;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Profile extends AppCompatActivity {
 
@@ -10,5 +16,15 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
+        TextView nameView = (TextView) findViewById(R.id.nameView);
+        nameView.setText("Quim Barreiros");
+
+        ListView lstView = (ListView) findViewById(R.id.profile_info);
+        List<String> options =  new ArrayList<>();
+        options.add("Age: 71");
+        options.add("Sex: Muito");
+        options.add("Bio: Sempre tive um especial afeto por gado caprino.");
+
+        lstView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, options));
     }
 }
